cfbe38b51746f810804ccc8a8b6f6f40d6c8391f
hirankumar <hirankailas@gmail.com>
2019-10-08 23:56:09 +0530

    test file changed

--- util.test.js
+++ util.test.js
@@ -2,5 +2,5 @@ const {GenerateText} = require('./util');
 
 test('Should be Hello world',async () =>{
     let text = await GenerateText();
-    expect(text).toBe('Hello world');
+    expect(text).toBe('Hello world1');
 });
